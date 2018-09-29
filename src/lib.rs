1a5513f15b6010b4b7af7f1e56e6364da63a42a4
Marek G <marek@nomail.com>
2018-09-29 22:01:35 +0200

    Added quantity parser.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate nom;
 extern crate chrono;
+extern crate rust_decimal;
 
 pub use self::parser::*;
 
--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,8 @@
 use std::str::FromStr;
 use nom::*;
+use nom::types::CompleteStr;
 use chrono::NaiveDate;
+use rust_decimal::Decimal;
 
 pub enum CustomError {
     NonExistingDate
@@ -10,26 +12,22 @@ fn is_digit(c: char) -> bool {
     (c >= '0' && c <= '9')
 }
 
-named!(number4<&str, i32>,
-    map_res!(take_while_m_n!(4, 4, is_digit), i32::from_str)
+named_args!(numberN(n: usize)<CompleteStr, i32>,
+    map_res!(take_while_m_n!(n, n, is_digit), |s: CompleteStr| { i32::from_str(s.0) })
 );
 
-named!(number2<&str, i32>,
-    map_res!(take_while_m_n!(2, 2, is_digit), i32::from_str)
-);
-
-named!(parse_date_internal<&str, (i32, i32, i32)>,
+named!(parse_date_internal<CompleteStr, (i32, i32, i32)>,
     do_parse!(
-        year: number4 >>
+        year: call!(numberN,4) >>
         alt!(tag!("-") | tag!("/") | tag!(".")) >>
-        month: number2 >>
+        month: call!(numberN,2) >>
         alt!(tag!("-") | tag!("/") | tag!(".")) >>
-        day: number2 >>
+        day: call!(numberN,2) >>
         ((year, month, day))
     )
 );
 
-pub fn parse_date(text: &str) -> IResult<&str, NaiveDate> {
+pub fn parse_date(text: CompleteStr) -> IResult<CompleteStr, NaiveDate> {
     let res = parse_date_internal(text)?;
 
     let rest = res.0;
@@ -39,9 +37,22 @@ pub fn parse_date(text: &str) -> IResult<&str, NaiveDate> {
     if let Some(parsed) = parsed_opt {
         Ok((rest, parsed))
     } else {
-        Err(Err::Error(error_position!(&text[0..10], ErrorKind::Custom(CustomError::NonExistingDate as u32))))
+        Err(Err::Error(error_position!(CompleteStr(&text.0[0..10]), ErrorKind::Custom(CustomError::NonExistingDate as u32))))
     }
 }
+
+named!(parse_quantity<CompleteStr, Decimal>,
+    map_res!(
+        recognize!(
+            tuple!(
+                opt!(tag!("-")),
+                digit,
+                opt!(tuple!(tag!("."), digit))
+            )
+        ),
+        |s: CompleteStr| { Decimal::from_str(s.0) }
+    )
+);
 
 #[cfg(test)]
 mod tests {
@@ -49,14 +60,18 @@ mod tests {
     use nom::ErrorKind::Custom;
     use nom::Context::Code;
     use nom::Err::Error;
+    use nom::types::CompleteStr;
 
     #[test]
     fn parse_date_test() {
-        assert_eq!(Ok(("", 2010)), number4("2010"));
-        assert_eq!(Ok(("", 20)), number2("20"));
-        assert_eq!(Ok(("", NaiveDate::from_ymd(2017, 03, 24))), parse_date("2017-03-24"));
-        assert_eq!(Ok(("", NaiveDate::from_ymd(2017, 03, 24))), parse_date("2017/03/24"));
-        assert_eq!(Ok(("", NaiveDate::from_ymd(2017, 03, 24))), parse_date("2017.03.24"));
-        assert_eq!(Err(Error(Code("2017-13-24", Custom(CustomError::NonExistingDate as u32)))), parse_date("2017-13-24"));
+        assert_eq!(Ok((CompleteStr(""), NaiveDate::from_ymd(2017, 03, 24))), parse_date(CompleteStr("2017-03-24")));
+        assert_eq!(Ok((CompleteStr(""), NaiveDate::from_ymd(2017, 03, 24))), parse_date(CompleteStr("2017/03/24")));
+        assert_eq!(Ok((CompleteStr(""), NaiveDate::from_ymd(2017, 03, 24))), parse_date(CompleteStr("2017.03.24")));
+        assert_eq!(Err(Error(Code(CompleteStr("2017-13-24"), Custom(CustomError::NonExistingDate as u32)))), parse_date(CompleteStr("2017-13-24")));
+    }
+
+    #[test]
+    fn parse_quantity_test() {
+        assert_eq!(Ok((CompleteStr(""), Decimal::new(202, 2))), parse_quantity(CompleteStr("2.02")));
     }
 }
